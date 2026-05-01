399c0d5e40adaa38a08b114e70f9dcfe69bfcefd
aleksandr.z <gently.whitesnow@outlook.com>
2026-05-01 15:52:05 +0300

    fix di

--- apps/api/src/Throne.Application/DependencyInjection.cs
+++ apps/api/src/Throne.Application/DependencyInjection.cs
@@ -10,6 +10,7 @@ public static class DependencyInjection
         services.AddSingleton(TimeProvider.System);
         services.AddScoped<CreateIntentHandler>();
         services.AddScoped<GetIntentHandler>();
+        services.AddScoped<ReadIntentTextHandler>();
         services.AddScoped<ReplaceIntentTextHandler>();
         services.AddScoped<ListIntentsHandler>();
         return services;
